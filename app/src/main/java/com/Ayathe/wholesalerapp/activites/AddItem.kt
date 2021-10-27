e2067a76a7d56b1256e40f99f91003552d188be3
Kuba-Brozek <kubabrozek9@gmail.com>
2021-10-27 20:14:02 +0200

    modified add item mostly

--- app/src/main/java/com/Ayathe/wholesalerapp/activites/AddItem.kt
+++ app/src/main/java/com/Ayathe/wholesalerapp/activites/AddItem.kt
@@ -1,17 +1,27 @@
 package com.Ayathe.wholesalerapp.activites
 
+import android.app.ProgressDialog
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
 import com.Ayathe.wholesalerapp.R
+import com.Ayathe.wholesalerapp.repository.FirebaseRepository
+import com.bumptech.glide.Glide
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_add_item.*
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 
 
 class AddItem : AppCompatActivity() {
@@ -19,8 +29,22 @@ class AddItem : AppCompatActivity() {
     val db = Firebase.firestore
     private val cloud = FirebaseFirestore.getInstance()
     val ref = db.collection("cars").document()
+    private val storage = FirebaseStorage.getInstance()
     private var firebaseStore: FirebaseStorage? = null
     private var storageReference: StorageReference? = null
+    private val REPO_DEBUG = "REPO_DEBUG"
+    private val mStorageRef = FirebaseStorage.getInstance().reference
+    private lateinit var mProgressDialog: ProgressDialog
+    private val REQUEST_IMAGE_CAPTURE = 1
+
+    private val repository = FirebaseRepository()
+    private var storageRef = storage.reference
+    private val PROFILE_DEBUG = "PROFILE_DEBUG"
+    private lateinit var database: DatabaseReference
+    private var filePath: Uri? = null
+    private val btnUpload: AppCompatButton by lazy {
+        findViewById(R.id.submititem) }
+
     val Any.TAG: String
         get() {
             val tag = javaClass.simpleName
@@ -31,9 +55,9 @@ class AddItem : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
 
-        submititem.setOnClickListener(View.OnClickListener {
+        submititem.setOnClickListener{
             setupSubmitDataClick()
-        })
+        }
         firebaseStore = FirebaseStorage.getInstance()
         storageReference = FirebaseStorage.getInstance().reference
 
@@ -65,6 +89,7 @@ class AddItem : AppCompatActivity() {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
+
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error adding document", e)
             }
